63dbcb48e6362b35646ab4692715baa4c6947bdc
sido <imbilusydney09@gmail.com>
2024-08-26 14:35:46 +0300

    implemented error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -16,25 +16,55 @@ app.use(cookieParser());
 app.use(express.json());
 
 if (!isProduction) {
-  app.use(cors());
+    app.use(cors());
 }
 
 app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin",
-  })
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin",
+    })
 );
 
 app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "lax",
-      httpOnly: true,
-    },
-  })
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "lax",
+            httpOnly: true,
+        },
+    })
 );
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource not found";
+    err.errors = ["The reuested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+const { ValidationError } = require("sequelize");
+
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = "Validation error";
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
